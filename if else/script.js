598325638cd6cceb0515e4fd995249ee31fd738b
rishikapathak2001 <rishikapathak2001@gmail.com>
2024-12-30 17:00:31 +0530

    if else

---
+++ if else/script.js
@@ -0,0 +1,90 @@
+// Question 1: Check Even or Odd
+// Prompt: Write a program that asks the user for a number and checks whether the number 
+// is even or odd using an if-else statement. Print "Even" if the number is even and "Odd" if the number is odd.
+// Hint: Use the modulus operator % to determine the remainder when dividing by 
+
+
+// var num = prompt("Enter a number:");
+// if (num % 2 === 0) {
+//     console.log("Even");
+//  } else {
+//     console.log("Odd");
+// }
+
+// Question 2: Simple Calculator
+// Prompt: Create a simple calculator that takes two numbers and an operator (+, -, *, /) as input from the user. 
+// Use if-else statements to perform the appropriate arithmetic operation and display the result.
+// Hint: Use if or else if to check which operator is entered and perform the corresponding calculation.
+
+// var num1 = prompt("enter no. 1:");
+// var num2 = prompt("enter no. 2:");
+// var operator = prompt("enter operator+, -, *, / = ");
+// var result ;
+
+// if (operator === "+") {
+//     result = num1 + num2;
+// } else if (operator === "-") {
+//     result = num1 - num2;
+// } else if (operator === "*") {
+//     result = num1 * num2;
+// } else if (operator === "/") {
+   
+//     result = num1 / num2;
+// } 
+
+// console.log(  result);
+
+
+
+
+
+//3. Compare Two Numbers
+// Prompt: Write a program that takes two numbers as input from the user.
+//  Use if-else statements to determine which number is greater, or if they are equal, and print the appropriate message.
+// Hint: Use if, else if, and else to compare the two numbers.
+
+
+
+// var num1 = prompt("enter 1st num:");
+// var num2 = prompt("enter 2nd num:");
+
+// if (num1 > num2) {
+//     console.log("first no. bada hai");
+// } else if (num1 < num2) {
+//     console.log("second no. bada hai");
+//  } else {
+//     console.log("dono equal hai");
+// }
+
+// Grade Calculation
+// Prompt: Write a program that calculates the grade of a student based on their score.
+//  Take a number as input (0-100), and use if-else statements to print the grade based on the following criteria:
+// 90-100: A
+// 80-89: B
+// 70-79: C
+// 60-69: D
+// Below 60: F
+// Hint: Use a series of if, else if, and else statements to check the score range.
+
+
+// var score = (prompt("enter marks:"));
+
+// if (score >= 90 & score <= 100){
+//     console.log("Grade: A");
+   
+
+//  } else if (score >= 80 && score < 90) {
+//     console.log("Grade: B");
+
+// } else if (score >= 70 && score < 80) {
+//     console.log("Grade: C");
+
+// } else if (score >= 60 && score < 70) {
+//     console.log("Grade: D");
+
+// } else if ( score < 60) {
+//     console.log("Grade: F");
+// }else{
+//     console.log("invaild")
+// }
+
